15c49f3b1aac93389f759e12307beb854c65af2e
Jenna Currin <jennacurrin@gmail.com>
2019-09-06 17:06:33 -0400

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+<script type="text/javascript"> 
+
+    var computerGuess= [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        console.log()
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
+        
+    }
+
+
+
+
+
+
+</script>
