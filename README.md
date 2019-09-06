a587141e43ab5d82b3481229a9a9a2a353dd6ebc
Jenna Currin <jennacurrin@gmail.com>
2019-09-06 19:06:22 -0400

    adding variables

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Psychic-Game
 Homework #3
+
+ Set variables;
+    userGuess
+    computerGuess
+    userWins= 0
+    userLoss= 0
+
+1. Computer guess is set
+2. User inputs guess
+3. Compare user guess and computer guess
+4. 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,20 @@
 <script type="text/javascript"> 
 
-    var computerGuess= [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-
-    document.onkeyup = function (event) {
-        var userGuess = event.key;
-        console.log()
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+   var computerGuess= ['abcdefghijklmnopqrstuvwxyz'.split('')];
+   var wins =0;
+   var losses= 0;
+   var userGuess = event.key;
 
 
 
-        
-    }
+    {/* This tells the computer to execute the script (whats in between the squiggle brackets) whenever a key is pressed  */}
+   
+   
+   object.onkeyup = function(){
+       
+   };
+
+
 
 
 
@@ -18,3 +22,22 @@
 
 
 </script>
+
+ {/* var computerGuess= [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+    var wins= 0;
+    var losses= 0; 
+
+
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        // console.log()
+        // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        // if ((userGuess === computerGuess;)){
+
+        // alert("User guess: " + userGuess)
+        // alert("Computer guess: " + computerGuess);
+
+
+        
+    }} */}
