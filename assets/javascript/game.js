0f00954c6ec2d877b856e7c7a6d50ff8732c9710
Jenna Currin <jennacurrin@gmail.com>
2019-09-13 17:15:50 -0400

    final game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-var computerChoices = ['a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'];
-//    var computerChoices= 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'.split; may delete
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -11,168 +10,53 @@ var remainingGuessesElement = document.getElementById("numGuesses");
 var whatUserGuessElement = document.getElementById("playersGuess")
 
 // create a function that chooses computer's guess 
-var computerGuess = function () {
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess);
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// var computerGuess = computerChoices[0];
+console.log(computerGuess);
+
+var restart = function () {
+    guessesLeft = 9;
+    letterUser = [];
+    losses = 0;
+    wins = 0;
 }
 
 document.onkeyup = function (event) {
+    console.log(computerGuess);
+    
     var userGuess = event.key;
-    console.log(userGuess)
-    if (userGuess === computerGuess) {
-        wins++;
-        document.querySelector("#numWins").innerHTML = "Wins: " + wins;
+    if (guessesLeft > 0) {
+        letterUser.push(userGuess);
+        whatUserGuessElement.innerHTML = letterUser.join(", ");
+
+        remainingGuessesElement.innerHTML = guessesLeft;
+
+        console.log(userGuess)
+        if (userGuess === computerGuess) {
+            wins++;
+            guessesLeft--;
+            winsElement.innerHTML = " " + wins;
+            // restart();
+
+        }
+        // else if (userGuess != computerGuess) {
+        //     guessesLeft -= 1;
+        // }
+        else {
+            losses++;
+            guessesLeft--;
+            lossElement.innerHTML = " " + losses;
+            // restart();
+        }
+    } else {
         restart();
-        // letterUser.push(userGuess);
-    } 
-    else if (userGuess != computerGuess) {
-        guessesLeft -= 1;
-    }
-    if (guessesLeft === 0) {
-        losses++;
-        document.querySelector("#numlosses").innerHTML = "Loses: " + losses;
-        restart();
-    }
-
-
-
-    var restart = function () {
-        guessesLeft = 9;
-        letterUser = [];
     }
 
     // var reset = function () {
 
     // }
 }
-computerGuess()
+// computerGuess()
 
 // function that resets the game 
 // update the number of guesses used
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    function countGuessesLeft() {
-// 	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-// }
-
-
-// function farUserGuesses() {
-// 	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
-// }
-
-// countGuessesLeft();
-
-// var restart = function() {
-// 	guessesLeft = 9;
-//     letterUser = [];
-// }
-
-// document.onkeyup = function(event) {
-// 	guessesLeft--;
-
-// 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-// 	letterUser.push(userGuess);
-// 	countGuessesLeft();
-// 	farUserGuesses();
-
-// 	if (userGuess === computerGuess){
-// 		numwins++;
-// 		document.querySelector("#numWins").innerHTML = "Wins: " + wins;
-// 		restart();
-// 	} 
-// 	else if (guessesLeft === 0) {
-// 		losses++;
-// 		document.querySelector("#lose").innerHTML = "Loses: " + losses;
-// 		restart();
-// 	}
-//   };
-
-
-
-
-
-//     {/* This tells the computer to execute the script (whats in between the squiggle brackets) whenever a key is pressed  */}
-
-
-//    document.onkeyup = function(event){
-//        var userGuess = event.key;
-//        console.log("event", event)
-//        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-//    };
-
-
-
-
-
-
-
-
-
-{
-    /* var computerGuess= [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-        var wins= 0;
-        var losses= 0; 
-
-
-        document.onkeyup = function (event) {
-            var userGuess = event.key;
-            // console.log()
-            // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-            // if ((userGuess === computerGuess;)){
-
-            // alert("User guess: " + userGuess)
-            // alert("Computer guess: " + computerGuess);
-
-
-            
-        }} */
-}
